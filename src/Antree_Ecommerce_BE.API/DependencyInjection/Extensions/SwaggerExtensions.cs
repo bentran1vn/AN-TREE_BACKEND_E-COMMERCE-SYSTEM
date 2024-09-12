439a924a6127edbc6bd192bd3cf4b1a3c6ffe604
bentran1vn <tan182205@gmail.com>
2024-09-12 09:43:56 +0700

    Config Adding Token Swagger

--- src/Antree_Ecommerce_BE.API/DependencyInjection/Extensions/SwaggerExtensions.cs
+++ src/Antree_Ecommerce_BE.API/DependencyInjection/Extensions/SwaggerExtensions.cs
@@ -1,5 +1,7 @@
 using Antree_Ecommerce_BE.API.DependencyInjection.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -9,7 +11,39 @@ public static class SwaggerExtensions
 {
     public static void AddSwaggerAPI(this IServiceCollection services)
     {
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme. 
+
+Enter 'Bearer' [space] and then your token in the text input below.
+
+Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+            
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        },
+                        Scheme = "oauth2",
+                        Name = JwtBearerDefaults.AuthenticationScheme,
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
+        });
         services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
     }
 
