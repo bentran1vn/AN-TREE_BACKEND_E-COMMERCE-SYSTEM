53200da57e97ea410b659f0d73fab55e3558bc51
bentran1vn <tan182205@gmail.com>
2024-10-17 11:47:44 +0700

    Add SePay Payment 1

--- src/Antree_Ecommerce_BE.Application/UserCases/Commands/Orders/CreateSePayOrderCommandHandler.cs
+++ src/Antree_Ecommerce_BE.Application/UserCases/Commands/Orders/CreateSePayOrderCommandHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Antree_Ecommerce_BE.Contract.Abstractions.Messages;
 using Antree_Ecommerce_BE.Contract.Abstractions.Shared;
 using Antree_Ecommerce_BE.Contract.Services.Orders;
@@ -21,8 +22,18 @@ public class CreateSePayOrderCommandHandler : ICommandHandler<Command.CreateSePa
 
         var order = await _orderRepository.FindByIdAsync(id, cancellationToken);
 
-        order.Status = 4;
-        order.Note = request.content + "-" + request.code;
+        order.Status = 5;
+
+        var orderId = "";
+        string note = request.content;
+        Match match = Regex.Match(note, @"QR\s+([^\s]+)");  // Regular expression to match non-space characters after "QR"
+
+        if (match.Success)
+        {
+            orderId = match.Groups[1].Value;
+        }
+        order.Note = orderId + "-" + request.gateway + "" + request.accountNumber + "-" + request.transferAmount;
+        // QR   orderId12345- Ma GD ACSP/ qG580192-
 
         return Result.Success("Oker");
     }
